1d44bf1ab469a5156a6f8347bc7e422fe2169c42
myc0l <miketjnr@gmail.com>
2019-03-27 23:14:27 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var expres = require("express");
+var router = express.Rouer();
+var passport = require("passport");
+var User = require("../models/user");
+
+//root route
+router.get("/", function(req, res){
+    res.render("landing");
+});
+
+// show register form
+router.get("/register", function(req, res){
+    res.render("register");
+});
+
+//handle sign up logic
+router.post("/register", function(req,res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            req.flash("error",err.message);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req,res, )
+    })
+})
